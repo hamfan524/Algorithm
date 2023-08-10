a60b5c805e1763004cc627c89acc3ec25962beff
Choe Dong Ho <hamfan524@naver.com>
2023-08-10 20:33:40 +0900

    [level 3] Title: 네트워크, Time: 0.67 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def bfs(n, computers, j, visited):
+    visited[j] = True
+    q = deque()
+    q.append(j)
+    while q:
+        temp = q.popleft()
+        visited[temp] = True
+        for i in range(n):
+            if i != temp and computers[temp][i] == 1:
+                if visited[i] == False:
+                    q.append(i)
+
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+    for i in range(n):
+        if visited[i] == False:
+            bfs(n, computers, i, visited)
+            answer += 1
+    return answer
