76e825cdbd88a8de2323747fbda3f716531adc3b
Choe Dong Ho <hamfan524@naver.com>
2023-08-10 19:12:37 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 3924 ms, Memory: 65372 KB -BaekjoonHub

---
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.py
@@ -0,0 +1,34 @@
+import sys
+from heapq import heappush, heappop
+
+input = sys.stdin.readline
+inf = sys.maxsize
+
+n, m, k = map(int, input().split())
+graph =[[] for _ in range(n + 1)]
+dp = [[inf] * k for _ in range(n + 1)]
+heap = []
+
+def dijkstra(start):
+    heappush(heap, [0, start])
+    dp[start][0] = 0
+    while heap:
+        w, n = heappop(heap)
+        for n_n, wei in graph[n]:
+            n_w = wei + w     
+            if n_w < dp[n_n][k-1]:
+                dp[n_n][k-1] = n_w
+                dp[n_n] = sorted(dp[n_n])
+                heappush(heap, [n_w, n_n])
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append([b, c])
+
+dijkstra(1)
+
+for i in range(1, n + 1):
+    if dp[i][k-1] == inf:
+        print(-1)
+    else:
+        print(dp[i][k-1])
