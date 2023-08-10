63ee9800b236e76d4af13c76dc0bfdcd483de42f
Choe Dong Ho <hamfan524@naver.com>
2023-08-10 19:52:23 +0900

    [Bronze II] Title: 음계, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,11 @@
+ascending = [1, 2, 3, 4, 5, 6, 7, 8]
+descending = [8, 7, 6, 5, 4, 3, 2, 1]
+
+number = list(map(int, input().split()))
+
+if number == ascending:
+    print("ascending")
+elif number == descending:
+    print("descending")
+else:
+    print("mixed")
