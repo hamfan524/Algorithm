8cdba0c860300f70b2007792970bd7b43a5f02d8
Choe Dong Ho <hamfan524@naver.com>
2023-08-10 20:30:25 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -0,0 +1,35 @@
+def dfs(graph, n, path, now):
+    path.append(now)
+
+    if len(path) == n + 1:
+        return True
+
+    if now not in graph:
+        path.pop()
+        return False
+    
+    for _ in range(len(graph[now])):
+        next = graph[now].pop()    
+        
+        if dfs(graph, n, path, next):
+            return True
+        
+        graph[now].insert(0, next)
+    
+    path.pop()
+    return False
+
+def solution(tickets):
+    answer = []
+    graph = {}
+    for start, end in tickets:
+        graph[start] = graph.get(start, []) + [end]
+    
+    for i in graph.keys():
+        graph[i].sort(reverse=True)
+    
+    path = []
+    if dfs(graph, len(tickets), path, "ICN"):
+        answer = path
+
+    return answer
