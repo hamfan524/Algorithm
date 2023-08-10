9fd124d612b4ac7b966c02b5806205f35f744b4a
Choe Dong Ho <hamfan524@naver.com>
2023-08-10 22:19:33 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,15 @@
+def solution(name):
+    if set(name) == {'A'}:
+        return 0
+    
+    answer = float('inf')
+    for i in range(len(name)//2 + 1):
+        l_r = name[-i:] + name[:-i] 
+        r_l = name[i::-1] + name[i+1:][::-1]
+        for n in [l_r, r_l]:
+            while n and n[-1] == 'A':
+                n = n[:-1]
+            cnt = [min(ord(c) - ord('A'), ord('Z') - ord(c) + 1) for c in n ]
+            answer = min(answer, i + (len(cnt)-1) + sum(cnt))
+
+    return answer
