106af566d8bffa04baeea1bf5df656c141663f01
Choe Dong Ho <hamfan524@naver.com>
2023-08-14 23:51:22 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,7 @@
+def solution(brown, yellow):
+    num = brown + yellow
+    for i in range(num, 2, -1):
+        if num % i == 0:
+            a = num // i
+            if yellow == (i - 2) * (a - 2):
+                return [i, a]
