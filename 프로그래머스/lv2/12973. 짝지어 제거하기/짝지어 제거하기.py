8fd885a5b72fb08504bb58ef25a485754797ec18
Choe Dong Ho <hamfan524@naver.com>
2023-08-15 00:40:14 +0900

    [level 2] Title: 짝지어 제거하기, Time: 96.26 ms, Memory: 13.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+def solution(s):
+    q = deque()
+    for i in s:
+        if not q:
+            q.append(i)
+        else:
+            if q[-1] == i:
+                q.pop()
+            else:
+                q.append(i)
+    
+    return 1 if not q else 0
