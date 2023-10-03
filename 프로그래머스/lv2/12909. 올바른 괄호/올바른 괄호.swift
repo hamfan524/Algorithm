e3fddce468d468495cb6d5312bef5ec45bfee34e
Choe Dong Ho <hamfan524@naver.com>
2023-10-03 19:54:56 +0900

    [level 2] Title: 올바른 괄호, Time: 10.75 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func solution(_ s:String) -> Bool
+{
+    var stack = [Character]()
+    
+    for char in s {
+        if char == "(" {
+            stack.append(char)
+        } else if char == ")" {
+            if stack.isEmpty {
+                return false
+            }
+            stack.removeLast()
+        }
+    }
+    
+    return stack.isEmpty
+}
