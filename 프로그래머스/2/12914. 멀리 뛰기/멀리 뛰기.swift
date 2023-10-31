82bb99bf21590e9cced75b46f2f71c38e41c26ee
Choe Dong Ho <hamfan524@naver.com>
2023-10-31 19:18:47 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.swift
@@ -0,0 +1,10 @@
+func solution(_ n:Int) -> Int {
+    var dp = [Int](repeating: 0, count: n + 1)
+    dp[0] = 1
+    dp[1] = 1
+    
+    for i in 2..<dp.count {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+    }
+    return dp[n]
+}
