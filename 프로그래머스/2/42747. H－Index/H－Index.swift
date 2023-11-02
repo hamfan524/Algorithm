687619db10425f5b0d0309513d6ed63d3208ecb5
Choe Dong Ho <hamfan524@naver.com>
2023-11-02 10:45:24 +0900

    [level 2] Title: H-Index, Time: 0.16 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.swift
+++ 프로그래머스/2/42747. H－Index/H－Index.swift
@@ -1,11 +1,20 @@
 import Foundation
 
+// func solution(_ citations:[Int]) -> Int {
+//     let sortedCitations = Array(citations.sorted().reversed())
+//     for i in 0..<sortedCitations.count {
+//         if sortedCitations[i] < i + 1 {
+//             return i
+//         }
+//     }
+//     return sortedCitations.count
+// }
+
 func solution(_ citations:[Int]) -> Int {
-    let sortedCitations = Array(citations.sorted().reversed())
-    for i in 0..<sortedCitations.count {
-        if sortedCitations[i] < i + 1 {
-            return i
+    for (index, value) in citations.sorted(by: > ).enumerated() {
+        if index >= value {
+            return index
         }
-    }
-    return sortedCitations.count
+    }   
+    return citations.count
 }
