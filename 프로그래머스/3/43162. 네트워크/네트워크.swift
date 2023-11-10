7bb81a70514ea6c75885b8596829e95356ceb154
Choe Dong Ho <hamfan524@naver.com>
2023-11-10 17:34:08 +0900

    [level 3] Title: 네트워크, Time: 0.39 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.swift
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -1,26 +1,25 @@
-func bfs(_ n: Int, _ computers: [[Int]], _ j: Int, _ visited: inout [Bool]) {
-    var queue = [j]
-    
-    while !queue.isEmpty {
-        let tmp = queue.removeFirst()
-        visited[tmp] = true
-        
-        for i in 0..<n {
-            if i != tmp && computers[tmp][i] == 1 && !visited[i] {
-                queue.append(i)
-            }
-        }
-    }
-
-}
-
 func solution(_ n: Int, _ computers: [[Int]]) -> Int {
     var answer = 0
     var visited = [Bool](repeating: false, count: n)
     
+    func bfs(_ j: Int) {
+        var queue = [j]
+
+        while !queue.isEmpty {
+            let tmp = queue.removeFirst()
+            visited[tmp] = true
+            
+            for i in 0..<n {
+                if computers[tmp][i] == 1 && !visited[i] {
+                    queue.append(i)
+                }
+            }
+        }
+    }
+    
     for i in 0..<n {
         if visited[i] == false {
-            bfs(n, computers, i, &visited)
+            bfs(i)
             answer += 1
         }
     }
